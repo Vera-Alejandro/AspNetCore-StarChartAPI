fcaec2fdb15c31434c0eeb71f41199a83a14dfc0
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-20 10:50:48 -0500

    Created Post, Put, Patch, and Delete for CelectrialObjectsController

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 using StarChart.Models;
@@ -62,6 +63,73 @@ namespace StarChart.Controllers
             }
 
             return Ok(allSpaceThings);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject spaceObject)
+        {
+            _context.CelestialObjects.Add(spaceObject);
+
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { id = spaceObject.Id }, spaceObject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject spaceObject)
+        {
+            CelestialObject foundSpaceThing = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+
+            if (foundSpaceThing == null)
+            {
+                return NotFound();
+            }
+
+            foundSpaceThing.Name = spaceObject.Name;
+            foundSpaceThing.OrbitalPeriod = spaceObject.OrbitalPeriod;
+            foundSpaceThing.OrbitedObjectId = spaceObject.OrbitedObjectId;
+
+            _context.CelestialObjects.Update(foundSpaceThing);
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            CelestialObject spaceThing = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+
+            if (spaceThing == null)
+            {
+                return NotFound();
+            }
+
+            spaceThing.Name = name;
+
+            _context.Update(spaceThing);
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            CelestialObject spaceThing = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+
+            if (spaceThing == null)
+            {
+                return NotFound();
+            }
+
+            _context.RemoveRange(spaceThing);
+
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
